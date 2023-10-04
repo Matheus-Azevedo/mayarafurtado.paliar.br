bc69309949e9282cc60091df91c4018f8356bcbd
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-10-04 10:31:16 -0300

    UPDATE: lading page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
 function LadingPage() {
   return (
     <div>
-      <h1>Landing Page</h1>
-      <p>This is the landing page</p>
+      <h1>Landing Page - mayarafurtado.paliar.br</h1>
+      <p>This is the landing page for physiotherapist Mayara Furtado.</p>
+      <p>Stay tuned for more content</p>
     </div>
   );
 }
